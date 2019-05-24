0e1aaf3cc1387840c93260e13d27a21a13afcad1
Jeremy Whiting <jpwhiting@kde.org>
2019-05-23 22:39:16 -0600

    Change DetailView to vertical splitter so Commit Message can be resized.
    Fixes issue T182.

--- src/ui/DetailView.cpp
+++ src/ui/DetailView.cpp
@@ -45,6 +45,7 @@
 namespace {
 
 const int kSize = 64;
+const QString kSplitterKey = "detailSplitter";
 const char *kCacheKey = "cache_key";
 const QString kRangeFmt = "%1..%2";
 const QString kDateRangeFmt = "%1-%2";
@@ -256,6 +257,7 @@ public:
     layout->addLayout(header);
     layout->addWidget(mSeparator);
     layout->addWidget(mMessage);
+    layout->addStretch();
 
     connect(&mMgr, &QNetworkAccessManager::finished,
             this, &CommitDetail::setPicture);
@@ -520,6 +522,7 @@ public:
     buttonLayout->addWidget(mStage);
     buttonLayout->addWidget(mUnstage);
     buttonLayout->addWidget(mCommit);
+    buttonLayout->addStretch();
 
     QHBoxLayout *layout = new QHBoxLayout(this);
     layout->setContentsMargins(0,0,0,12);
@@ -716,22 +719,35 @@ ContentWidget::~ContentWidget() {}
 DetailView::DetailView(const git::Repository &repo, QWidget *parent)
   : QWidget(parent)
 {
-  QVBoxLayout *layout = new QVBoxLayout(this);
+  QHBoxLayout *layout = new QHBoxLayout(this);
   layout->setContentsMargins(0,0,0,0);
-  layout->setSpacing(0);
+
+  QSplitter *splitter = new QSplitter(this);
+  splitter->setHandleWidth(0);
+  connect(splitter, &QSplitter::splitterMoved, [this, splitter] {
+    QSettings().setValue(kSplitterKey, splitter->saveState());
+  });
+
+  layout->addWidget(splitter);
+  splitter->setContentsMargins(0,0,0,0);
+  splitter->setOrientation(Qt::Vertical);
 
   mDetail = new StackedWidget(this);
   mDetail->setVisible(false);
-  layout->addWidget(mDetail);
+  splitter->addWidget(mDetail);
 
   mDetail->addWidget(new CommitDetail(this));
   mDetail->addWidget(new CommitEditor(repo, this));
 
   mContent = new QStackedWidget(this);
-  layout->addWidget(mContent, 1);
+  splitter->addWidget(mContent);
 
   mContent->addWidget(new DiffWidget(repo, this));
   mContent->addWidget(new TreeWidget(repo, this));
+
+  // Restore splitter state.
+  splitter->restoreState(QSettings().value(kSplitterKey).toByteArray());
+
 }
 
 DetailView::~DetailView() {}
